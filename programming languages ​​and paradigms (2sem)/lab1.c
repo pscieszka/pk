ad2c873d425f5ddaaed6bb1eeaaae0b46b2ad7ee
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-03-03 12:28:44 +0100

    bruh

--- programming languages ​​and paradigms (2sem)/lab1.c
+++ programming languages ​​and paradigms (2sem)/lab1.c
@@ -17,7 +17,7 @@ scanf("%f", &f);
 scanf("%lf%lf", &d1, &d2);
 //Output data
 printf("Liczby calkowite\n---------------------\n");
-printf("i1 |%d|\t\td\n", i1);
+printf("int |%d|\t\td\n", 1);
 printf("i1 |%10d|\t\t10d\n", i1);
 printf("i1 |%u|\t\tu\n", i1);
 printf("i2 |%-10d|\t\t-10d\n", i2);
@@ -25,7 +25,7 @@ printf("s1 |%010hd|\t\t010hd\n", s1);
 printf("s1 |%010hu|\t\t010hu\n", s1);
 printf("l1 |%+10ld|\t\t+10ld\n", l1);
 //printf("i1 |%-010d|\t\t-010d\n", i1);
-printf("\nLiczby typu float\n---------------------\n");
+printf("\nLiczby rzeczywiste\n---------------------\n");
 printf("zz |%f|\t\tf\n", zz);
 printf("xx |%10.5f|\t\t10.5f\n", xx);
 printf("qq |%-+10.5lf|\t\t-+10.5f\n", qq);
@@ -39,5 +39,7 @@ for(int it=0; it<10; ++it)
  dd = 100.0*(it+1);
  printf("|%10d|%20.8lf|%+20.8le|\n", it, dd, dd);
 }
+int ch;
+while((ch = getchar()) != EOF);
 return 0;
 }
