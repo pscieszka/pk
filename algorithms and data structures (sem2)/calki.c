b568f6fb43994281a33abb9ac4b8868b99e563ec
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-03-07 21:36:51 +0100

    calki

---
+++ algorithms and data structures (sem2)/calki.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define lp 1000
+
+double c_od,c_do;
+
+double f3(double x)
+{
+ return -1;      
+}      
+double f4(double x) {
+
+ return 1;
+}
+
+
+double prostokaty(double( *f)(double))
+{
+       double krok, p, suma=0;
+       krok=(c_do-c_od)/lp;
+       for(p=c_od; p<c_do; p+=krok)
+               suma+=(*f)(p);
+               suma*=krok;
+       return suma;      
+}
+
+double trapezy(double (*f)(double))
+{
+return 1;
+}
+       
+double mc(double(*f)(double))
+{
+ return 1;
+}
+
