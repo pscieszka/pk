a36a31b0a36bb9a0db162e9f2a55edce91e8cea0
Piotr Ścieszka <114289131+pscieszka@users.noreply.github.com>
2023-04-29 12:36:12 +0000

    commit

--- Sorting_project/main.c
+++ Sorting_project/main.c
@@ -12,16 +12,21 @@ int main(){
     int n1,n2;
     printf("Podaj wielkosc tablicy dla grupy 1(max 10mln):");
     scanf("%d",&n1);
-    printf("Podaj wielkosc tablicy dla grupy 2(max 10mln):");
-    scanf("%d",&n2);
+   // printf("Podaj wielkosc tablicy dla grupy 2(max 10mln):");
+    //scanf("%d",&n2);
+    FILE *f = fopen("result.txt", "w");
+    stdout = f;
+    for(int i =n1/20; i<n1; i+=n1/20){
+        printf("\n test dla n= %d \n",i);
+        int *tab = CreateArray(i);
+        test1(tab,i);
+    }
+    fclose(f);
+    //tab = FreeArray(tab);
 
-    int *tab = CreateArray(n1);
-    test1(tab,n1);
-    tab = FreeArray(tab);
-
-    int *tab2 = CreateArray(n2);
-    test2(tab2,n2);
-    tab2 = FreeArray(tab2);
+   // int *tab2 = CreateArray(n2);
+    //test2(tab2,n2);
+   // tab2 = FreeArray(tab2);
     
   
 
