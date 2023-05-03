23e4fd5fd663bb289190437e2ecb67ca940c7e27
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-05-03 18:29:48 +0200

    almost done

--- Sorting_project/main.c
+++ Sorting_project/main.c
@@ -9,12 +9,26 @@ int main(){
     srand(time(NULL));
     clock_t start, end;
     double time;
-    int n;
-    printf("Podaj wielkosc tablicy dla grupy:");
+    int n, wybor;
+    
+    char file[64]; 
+    printf("Podaj wielkosc tablicy (int>0):");
     scanf("%d",&n);
+    if(n < 1){
+        printf("Nie ma takiej opcji");
+        return 0;
+    }
+    printf("\nPodaj nazwe pliku z danymi:");
+    scanf("%s",file);
+    printf("\nWybierz 0, 1.\n 0 - wypisanie posortowanych liczb przez printf \n 1 - tylko wyniki czasu dzialnia \n wybor:");
+    scanf("%d",&wybor);
+    if(wybor != 0 && wybor != 1 ){
+        printf("Nie ma takiej opcji");
+        return 0;
+    }
+    printf("\n");
+    
     int *tab = CreateArray(n);
-
-    test(tab,n,"random.txt");
-   
+    test(tab,n,file,wybor);
     tab = FreeArray(tab);
 }
