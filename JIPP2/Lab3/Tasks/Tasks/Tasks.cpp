e0d3606f46e72eb4adb7c4cfc471ccdc367e4af5
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-11-10 21:21:25 +0100

    taskNote ADDED

--- JIPP2/Lab3/Tasks/Tasks/Tasks.cpp
+++ JIPP2/Lab3/Tasks/Tasks/Tasks.cpp
@@ -1,8 +1,27 @@
 ﻿
 #include <iostream>
 
+#include "Task.h"
+#include "TaskNote.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+
+    Task* task = new TaskNote(6,"zadanie2","cos",false);
+ 
+    TaskNote* task2 = new TaskNote(5, "zadanie1", "Napisz klase", false);
+    task2->setTest(34);
+
+    Task* taskTest = task2;
+    taskTest->display();
+    taskTest = task;
+    taskTest->display();
+
+
+
+    
+    
 }
 
